744210687906f2cfd7aae8ccb9d2b47dde5e02dc
mrgreenstar <andrey.lalaev@gmail.com>
2018-10-08 22:54:16 +0300

    Nothing interesting

--- java/com/mrgreenstar/sn/SettingsController.java
+++ java/com/mrgreenstar/sn/SettingsController.java
@@ -20,10 +20,7 @@ public class SettingsController {
     @GetMapping("/settings")
     public ModelAndView settingsPage(Principal principal, ModelAndView model) {
         User user = userRepository.findUserByEmail(principal.getName());
-        model.addObject("ownerId", user.getId());
-        model.addObject("firstName", user.getFirstName());
-        model.addObject("lastName", user.getLastName());
-        model.addObject("country", user.getCountry());
+        model = addObjectsInModel(model, user);
         model.setViewName("settings");
         return model;
     }
@@ -32,14 +29,36 @@ public class SettingsController {
     public ModelAndView saveSettings(@RequestParam("firstName") String firstName,
                                      @RequestParam("lastName") String lastName,
                                      @RequestParam("country") String country,
+                                     @RequestParam("email") String email,
                                      Principal principal, ModelAndView model) {
         User user = userRepository.findUserByEmail(principal.getName());
-        user.setFirstName(firstName);
-        user.setLastName(lastName);
-        user.setCountry(country);
-        userRepository.save(user);
-        model.addObject("info", "Changes have been saved");
+        model = addObjectsInModel(model, user);
+        if (!user.getFirstName().equals(firstName)) {
+            user.setFirstName(firstName);
+        }
+        if (!user.getLastName().equals(lastName)) {
+            user.setLastName(lastName);
+        }
+        if (!user.getCountry().equals(country)) {
+            user.setCountry(country);
+        }
+        // We need to be sure that this email wasn't already taken by somebody else
+        if (!user.getEmail().equals(email) && userRepository.findUserByEmail(email) == null) {
+            user.setEmail(email);
+            userRepository.save(user);
+            model.addObject("info", "Changes have been saved");
+        }
+        else model.addObject("emailError", "Error was occurred. This email is already taken");
         model.setViewName("settings");
+        return model;
+    }
+
+    private ModelAndView addObjectsInModel(ModelAndView model, User user) {
+        model.addObject("ownerId", user.getId());
+        model.addObject("firstName", user.getFirstName());
+        model.addObject("lastName", user.getLastName());
+        model.addObject("country", user.getCountry());
+        model.addObject("email", user.getEmail());
         return model;
     }
 }
